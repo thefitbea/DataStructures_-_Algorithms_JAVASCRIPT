44e752214ee5a7dc1cc32882814863a1e17f482f
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-08-23 22:31:25 +0530

    working code
    With and Without using constructors

--- codeSnippets/advancedTipCalculator.js
+++ codeSnippets/advancedTipCalculator.js
@@ -1,75 +1,81 @@
-// var i,j;//Global variables
 
-// function EatOut(restaurantName,restaurantBill) {
-//     this.restaurantName= restaurantName;
-//     this.restaurantBill=restaurantBill;
-//     this.waiterTip=[];
-//     this.waiterTip= function(){
-//         for(i=0;i<this.waiterTip.length;i++){
-//             if(this.restaurantBill[i]<50){
-//                 this.restaurantBillWithTip[i]=(this.restaurantBill[i]*(20/100));
-//                 return this.restaurantBillWithTip[i];
+
+// var i,j,percentage,bill,totalTip,averageTip;// Declared as Global variables so that scope can be extended
+
+
+// var mark ={
+//     restaurantName: "restaurantA",
+//     restaurantBill: [124,48,268,180,42],
+//     calculation: function(){
+//         this.waiterTip=[];
+//         this.restaurantBillWithTip=[];
+//         for(i=0;i<this.restaurantBill.length;i++){
+//             var bill=this.restaurantBill[i];
+//             var percentage;
+//             if(bill<50){
+//                 percentage=(20/100);
 //             }
-//             else if(this.restaurantBill[i]>50 && this.restaurantBill[i]<200){
-//                 this.restaurantBillWithTip[i]=(this.restaurantBill[i]*(15/100));
-//                 return this.restaurantBillWithTip[i];
+//             else if(bill>50 && bill<200){
+//                 percentage=(15/100);
 //             }
 //             else{
-//                 this.restaurantBillWithTip[i]=(this.restaurantBill[i]*(10/100));
-//                 return this.restaurantBillWithTip[i];
+//                 percentage=(10/100);
 //             }
-//         };
-//         return this.waiterTip[i];
-//     };
-//     this.restaurantBillWithTip=[];
-//     this.restaurantBillWithTip=function(){
-//         this.restaurantBillWithTip[i]=(this.restaurantBill[i]+this.waiterTip[i]);
-//         return this.restaurantBillWithTip[i];
-//     }
-//     this.averageTip=function(){
-//         var totalTip=0;
-//         for(j=0;j<this.restaurantBillWithTip.length;j++){
-//             this.totalTip=(this.totalTip+this.restaurantBillWithTip[j]);
-//             this.averageTip=(this.totalTip/this.restaurantBillWithTip.length);
+//             this.waiterTip[i]=Math.round((bill*percentage));
+//             this.restaurantBillWithTip[i]=(bill+this.waiterTip[i]);
 //         }
-//         return this.averageTip;
-//     };
-// };
+//     }
+// }
 
-// *****************-not working as of now********************************
 
-var i,j;//Global variables
+// mark.calculation();
+
+
+//  function averageTipCalc(waiterTip){
+//         totalTip=0;
+//         for(j=0;j<waiterTip.length;j++){
+//             totalTip=(totalTip+waiterTip[j]);
+//         }
+//         return averageTip=(totalTip/waiterTip.length);
+//     }
+
+// mark.average=averageTipCalc(mark.waiterTip);
+
+// //Message to USER
+
+// console.log("Waiter tips are "+ mark.waiterTip+"respectively and total bill is "+ totalTip+". Also average tip is "+ mark.average);
+
+
+// **************************************USING CONSTRUCTOR********************************************
+
+var i,j,percentage,bill,totalTip,averageTip;// Declared as Global variables so that scope can be extended
 
 function EatOut(restaurantName,restaurantBill) {
     this.restaurantName= restaurantName;
     this.restaurantBill=restaurantBill;
-    this.Calculation= function(){
+    this.waiterTip=[];
+    this.calculation= function(){
         this.waiterTip=[];
         this.restaurantBillWithTip=[];
-        for(i=0;i<this.waiterTip.length;i++){
-            if(this.restaurantBill[i]<50){
-                this.waiterTip[i]=(this.restaurantBill[i]*(20/100));
-                this.restaurantBillWithTip[i]=(this.restaurantBill[i]+this.waiterTip[i]);
+        for(i=0;i<this.restaurantBill.length;i++){
+            var bill=this.restaurantBill[i];
+            var percentage;
+            if(bill<50){
+                percentage=(20/100);
             }
-            else if(this.restaurantBill[i]>50 && this.restaurantBill[i]<200){
-                this.waiterTip[i]=(this.restaurantBill[i]*(15/100));
-                this.restaurantBillWithTip[i]=(this.restaurantBill[i]+this.waiterTip[i]);
+            else if(bill>50 && bill<200){
+                percentage=(15/100);
             }
             else{
-                this.waiterTip[i]=(this.restaurantBill[i]*(10/100));
-                this.restaurantBillWithTip[i]=(this.restaurantBill[i]+this.waiterTip[i]);
+                percentage=(10/100);
             }
-        };
-        return this.waiterTip[i];
+            this.waiterTip[i]=Math.round((bill*percentage));
+            this.restaurantBillWithTip[i]=(bill+this.waiterTip[i]);
+        }
     };
-    
-   
 };
 
-
-var mark = new EatOut("A",[124,48,268,180,42]);
-
- function averageTipCalc(){
+function averageTipCalc(){
         var totalTip=0;
         for(j=0;j<this.restaurantBillWithTip.length;j++){
             this.totalTip=(this.totalTip+this.restaurantBillWithTip[j]);
@@ -77,6 +83,22 @@ var mark = new EatOut("A",[124,48,268,180,42]);
         }
     }
 
+
+
+var mark = new EatOut("a",[124,48,268,180,42]);
+
+mark.calculation();
+
+function averageTipCalc(waiterTip){
+        totalTip=0;
+        for(j=0;j<waiterTip.length;j++){
+            totalTip=(totalTip+waiterTip[j]);
+        }
+        return averageTip=(totalTip/waiterTip.length);
+    }
+
+mark.average=averageTipCalc(mark.waiterTip);
+
 //Message to USER
 
-console.log("Waiter tip is "+ mark+" and total bill is "+ mark+". Also average tip is "+ mark);
+console.log("Waiter tips are "+ mark.waiterTip+" respectively and total bill is "+ totalTip+". Also average tip is "+ mark.average);
